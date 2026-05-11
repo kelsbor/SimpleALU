9861b1e5009eb2244a3ff4d1bce8e869eda2cbdd
Tpotsk <tpotsk@gmail.com>
2026-05-11 00:50:40 +0000

    Starting multiplication 2bits

--- src/mul_2b.vhd
+++ src/mul_2b.vhd
@@ -2,5 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity mul_2b is
-	
+    port (
+        a, b: in std_logic_vector(3 downto 1);
+        mul :  out std_logic_vector(3 downto 1)
+    );
 end mul_2b;
+
+architecture multiplication of mul_2b is
+    signal s0,s1,s2 : std_logic;
+    begin
+    -- mul(0) <= a(0) AND b(0);
+    s0 <= a(1) AND b(0);
+    s1 <= a(0) AND b(1);
+    
+    
+end multiplication;
