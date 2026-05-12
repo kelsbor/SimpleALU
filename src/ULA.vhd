3a800573f5e698696131cfcafaf4d4616d990491
Kehsbor <kehsbor@gmail.com>
2026-05-11 22:13:24 -0300

    feat: Subtrator + Signed Display

--- src/ULA.vhd
+++ src/ULA.vhd
@@ -1,6 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use work.ULA_package.all;
+use work.ula_package.all;
 
 -- Arquivo central do projeto que une a lógica da ULA e a interface da placa
 
@@ -46,80 +46,80 @@ architecture interface of ULA is
 	
 	
 	with opcode select
-		HEX0 <=      "1000000" when "000",
-						 "1111001" when "001",
-                   "0100100" when "010",
-                   "0110000" when "011",
-                   "0011001" when "100",
-                   "0010010" when "101",
-                   "0000010" when "110",
-						 "1111111" when others;
+		HEX0 <=         "1000000" when "000",
+						"1111001" when "001",
+                        "0100100" when "010",
+                        "0110000" when "011",
+                        "0011001" when "100",
+                        "0010010" when "101",
+                        "0000010" when "110",
+						"1111111" when others;
 	
 	with opcode select
-		resfinal <=  res_And when "001",
-                   res_Not when "010",
-                   res_Or when "011",
-						 res_Soma when "100",
-						 res_Sub when "101",
-						 res_Mul when "110",
-						 "0000" when others;
+		resfinal <=     res_And when "001",
+                        res_Not when "010",
+                        res_Or when "011",
+						res_Soma when "100",
+						res_Sub when "101",
+						res_Mul when "110",
+						"0000" when others;
 						 
 						 
 	with resfinal select
-		Hex6 <= 		 "1000000" when x"0",
+		Hex6 <=    "1000000" when "0000", 
                    "1111001" when "0001",
-                   "0100100" when x"2",
-                   "0110000" when x"3",
-                   "0011001" when x"4",
-                   "0010010" when x"5",
-                   "0000010" when x"6",
-                   "1111000" when x"7",
-                   "0000000" when x"8",
-                   "0010000" when x"9",
-                   "0001000" when x"A",
-                   "0000011" when x"B",
-                   "1000110" when x"C",
-                   "0100001" when x"D",
-                   "0000110" when x"E",
-                   "0001110" when x"1111",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000", -- (-8)
+                   "1111000" when "1001", -- (-7)
+                   "0000010" when "1010", -- (-6)
+                   "0010010" when "1011", -- (-5)
+                   "0011001" when "1100", -- (-4)
+                   "0110000" when "1101", -- (-3)
+                   "0100100" when "1110", -- (-2)
+                   "1111001" when "1111", --(-1)
                    "1111111" when others;
 	
 	with A select
-		HEX4 <=  "1000000" when x"0",
-                   "1111001" when x"1",
-                   "0100100" when x"2",
-                   "0110000" when x"3",
-                   "0011001" when x"4",
-                   "0010010" when x"5",
-                   "0000010" when x"6",
-                   "1111000" when x"7",
-                   "0000000" when x"8",
-                   "0010000" when x"9",
-                   "0001000" when x"A",
-                   "0000011" when x"B",
-                   "1000110" when x"C",
-                   "0100001" when x"D",
-                   "0000110" when x"E",
-                   "0001110" when x"F",
+		HEX4 <=  "1000000" when "0000", 
+                   "1111001" when "0001",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000", -- (-8)
+                   "1111000" when "1001", -- (-7)
+                   "0000010" when "1010", -- (-6)
+                   "0010010" when "1011", -- (-5)
+                   "0011001" when "1100", -- (-4)
+                   "0110000" when "1101", -- (-3)
+                   "0100100" when "1110", -- (-2)
+                   "1111001" when "1111", --(-1)
                    "1111111" when others;
 
 	with B select
-		HEX2 <=  "1000000" when x"0",
-                   "1111001" when x"1",
-                   "0100100" when x"2",
-                   "0110000" when x"3",
-                   "0011001" when x"4",
-                   "0010010" when x"5",
-                   "0000010" when x"6",
-                   "1111000" when x"7",
-                   "0000000" when x"8",
-                   "0010000" when x"9",
-                   "0001000" when x"A",
-                   "0000011" when x"B",
-                   "1000110" when x"C",
-                   "0100001" when x"D",
-                   "0000110" when x"E",
-                   "0001110" when x"F",
+		HEX2 <=  "1000000" when "0000", 
+                   "1111001" when "0001",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000", -- (-8)
+                   "1111000" when "1001", -- (-7)
+                   "0000010" when "1010", -- (-6)
+                   "0010010" when "1011", -- (-5)
+                   "0011001" when "1100", -- (-4)
+                   "0110000" when "1101", -- (-3)
+                   "0100100" when "1110", -- (-2)
+                   "1111001" when "1111", --(-1)
                    "1111111" when others;
 	
 end interface;
--- src/somador_4b.vhd
+++ src/somador_4b.vhd
@@ -3,22 +3,27 @@ use ieee.std_logic_1164.all;
 use work.ula_package.full_adder;
 
 entity somador_4b is
-	port (
-		Cin: in std_logic;
-		A: in std_logic_vector(3 to 0);
-		B: in std_logic_vector(3 to 0);
-		soma : out std_logic_vector(3 to 0);
-		carry_out: out std_logic
-	);
+  port (
+    Cin       : in std_logic;
+    A         : in std_logic_vector(3 downto 0);
+    B         : in std_logic_vector(3 downto 0);
+    soma      : out std_logic_vector(3 to 0);
+    carry_out : out std_logic
+  );
 end somador_4b;
 
 architecture somar of somador_4b is
-	signal c0,c1,c2 : std_logic;
-	
-	begin
-	SO1 : full_adder port map (carry_in => Cin, bit_a=> A(0), bit_b=>B(0) , sum=> soma(0), carry_out => c0);
-	SO2 : full_adder port map (carry_in => c0, bit_a=> A(1), bit_b=>B(1) , sum=> soma(1), carry_out => c1);
-	SO3 : full_adder port map (carry_in => C1, bit_a=> A(2), bit_b=>B(2) , sum=> soma(2), carry_out => c2);
-	SO4 : full_adder port map (carry_in => C2, bit_a=> A(3), bit_b=>B(3) , sum=> soma(3), carry_out => carry_out);
-	
+  signal c0, c1, c2 : std_logic;
+  signal Bfinal     : std_logic_vector(3 downto 0);
+
+begin
+  with Cin select
+    Bfinal <= not B when '1',
+    B when others;
+
+  SO1 : full_adder PORT map (carry_in => Cin, bit_a => A(0), bit_b => Bfinal(0), sum => soma(0), carry_out => c0);
+  SO2 : full_adder PORT map (carry_in => c0, bit_a => A(1), bit_b => Bfinal(1), sum => soma(1), carry_out => c1);
+  SO3 : full_adder PORT map (carry_in => C1, bit_a => A(2), bit_b => Bfinal(2), sum => soma(2), carry_out => c2);
+  SO4 : full_adder PORT map (carry_in => C2, bit_a => A(3), bit_b => Bfinal(3), sum => soma(3), carry_out => carry_out);
+
 end somar;
